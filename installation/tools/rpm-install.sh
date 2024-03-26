036bcc2cdfcb1008dfcdb40101a2d786f8d9d99d
世界 <i@sekai.icu>
2024-03-26 15:27:29 +0000

    Update

---
+++ installation/tools/rpm-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+ARCH_RAW=$(uname -m)
+case "${ARCH_RAW}" in
+    'x86_64')    ARCH='amd64';;
+    'x86' | 'i686' | 'i386')     ARCH='386';;
+    'aarch64' | 'arm64') ARCH='arm64';;
+    'armv7l')   ARCH='armv7';;
+    's390x')    ARCH='s390x';;
+    *)          echo "Unsupported architecture: ${ARCH_RAW}"; exit 1;;
+esac
+
+VERSION=$(curl -s https://api.github.com/repos/SagerNet/serentry/releases/latest \
+    | grep tag_name \
+    | cut -d ":" -f2 \
+    | sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+
+curl -Lo serentry.rpm "https://github.com/SagerNet/serentry/releases/download/v${VERSION}/serentry_${VERSION}_linux_${ARCH}.rpm"
+sudo rpm -i serentry.rpm
+rm serentry.rpm
